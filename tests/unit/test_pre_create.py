87e7827006ef328016dd988dbe9b3d2c215b66b2
Karl Ritchie <ritchie.karl@gmail.com>
2025-03-06 11:29:14 -0500

    feat: Add unit test case for config select split

--- tests/unit/test_pre_create.py
+++ tests/unit/test_pre_create.py
@@ -1,17 +1,15 @@
 import pytest
 
 from dbt.adapters.starrocks.helpers.pre_create import (
-    is_pre_creatable,
+    is_pre_creatable, split_config_select,
 )
 
 
 class TestPreCreate:
+    # DBT-like statements
+    complex_dbt_str = """
 
-    @pytest.mark.parametrize("sql", [
-        # DBT-like statements
-        """
-  
-    
+
 
   create table `jaffle_shop`.`customers__dbt_tmp`
     PROPERTIES (
@@ -31,67 +29,47 @@ orders as (
 
 ),
 
-payments as (
-
-    select * from `jaffle_shop`.`stg_payments`
-
-),
-
-customer_orders as (
-
-        select
-        customer_id,
-
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
-    from orders
-
-    group by customer_id
-
-),
-
-customer_payments as (
-
-    select
-        orders.customer_id,
-        sum(amount) as total_amount
-
-    from payments
-
-    left join orders on
-         payments.order_id = orders.order_id
-
-    group by orders.customer_id
-
-),
-
 final as (
 
     select
         customers.customer_id,
         customers.first_name,
-        customers.last_name,
-        customer_orders.first_order,
-        customer_orders.most_recent_order,
-        customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
 
     from customers
 
     left join customer_orders
         on customers.customer_id = customer_orders.customer_id
 
-    left join customer_payments
-        on  customers.customer_id = customer_payments.customer_id
-
 )
 
 select * from final"""
-    ])
+
+    @pytest.mark.parametrize("sql", [complex_dbt_str])
     def test_is_pre_creatable(self, sql):
         assert is_pre_creatable(sql) == True
 
+    @pytest.mark.parametrize("sql", [complex_dbt_str])
+    def test_split_ok(self, sql):
+        config_split, select_split = split_config_select(sql)
+        assert config_split == '  create table `jaffle_shop`.`customers__dbt_tmp`    PROPERTIES (      "replication_num" = "1"    )  '
+        assert select_split == (
+            'as '
+            'with  customers as ('
+            '    select * from `jaffle_shop`.`stg_customers`'
+            '),'
+            'orders as ('
+            '    select * from `jaffle_shop`.`stg_orders`'
+            '),'
+            'final as ('
+            '    select'
+            '        customers.customer_id,'
+            '        customers.first_name,'
+            '    from customers'
+            '    left join customer_orders'
+            '        on customers.customer_id = customer_orders.customer_id'
+            ')'
+            'select * from final'
+        )
     @pytest.mark.parametrize("sql", [
         # DBT-like statements
         """select
